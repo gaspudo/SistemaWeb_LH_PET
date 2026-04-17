515b58e0bda770697cd10131ff0b117c5a21c267
Joao Gaspar <danjpjcisalh@gmail.com>
2026-04-17 01:44:33 -0300

    Switching packages versions + adding the corrects references to Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@ using System.Text;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LH_PET_WEB.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
@@ -10,8 +12,16 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using LH_PET_WEB.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ContextoBanco>(options =>
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))
+    ));
+
+builder.Services.AddScoped<IEmailService, EmailService>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -19,8 +29,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
     .AddCookie(options =>
     {
-        options.LoginPath = "/Account/Login";
-        options.AccessDeniedPath = "/Account/AccessDenied";
+        options.LoginPath = "/Autenticacao/Login";
+        options.AccessDeniedPath = "/Autenticacao/AcessoNegado";
         options.ExpireTimeSpan = TimeSpan.FromHours(8);
     });
 
